a4b8e2bba32541b47dcb434e302904e2a16f1f3a
yychen <yychen@yoozoo.com>
2018-08-03 16:59:38 +0800

    commit sample to 1.0.0

---
+++ Sample/app/src/main/java/com/mob/grow/sample/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mob.grow.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import com.mob.grow.gui.GROWGUI;
+import com.mob.tools.utils.DeviceHelper;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.main_growsdk).setOnClickListener(this);
+
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.main_growsdk:
+				String uid = "10"+DeviceHelper.getInstance(this).getSerialno();
+				EditText editText = (EditText) findViewById(R.id.etuid);
+				if (!TextUtils.isEmpty(editText.getText())) {
+					uid = editText.getText().toString();
+				}
+				String nickname = DeviceHelper.getInstance(this).getAppName() + " test";
+				String avatarUrl = "http://download.sdk.mob.com/2018/02/06/12/1517890066763/2000_2000_79.49.jpg";
+				GROWGUI.showNewsPage(this,uid, nickname, avatarUrl);
+				break;
+		}
+	}
+
+}
